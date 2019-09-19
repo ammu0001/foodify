54a315bc52adb93c452b094793edcd4c1555a276
AMAN SHARMA <amansharmaammu2@gmail.com>
2019-09-19 19:42:23 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo, { ReactComponent } from './logo.svg';
 import './App.css';
+import Form from "./components/Form";
 
-function App() {
+class App extends Component {
+
+	getRecipe(e) {
+
+	    const recipeName = e.target.elements.recipeName.value;
+	    e.preventDefault();
+		console.log(recipeName);
+	}
+
+render(){
   return (
-    <div className="App">
+
+    <div className ="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Foodify</h1>
       </header>
+      < Form getRecipe={this.getRecipe} />
     </div>
+
   );
+}
+
 }
 
 export default App;
